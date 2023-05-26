4e101c39bab23e4c506f488cd3200c7061c582b2
Buğra Naim Acar <47912949+bugranaimacar@users.noreply.github.com>
2023-05-26 20:59:59 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -34,5 +34,61 @@ async function getMeals() {
       console.log('Error getting meals:', error);
     }
   }
+
+  async function getRecycleStations() {
+    const url = "https://climate-change-in-tedu-campus-default-rtdb.europe-west1.firebasedatabase.app/recyclinglocations.json";
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      // Loop through the data and create HTML elements for each recycle station
+      // Loop through the data and create HTML elements for each recycle station
+        Object.values(data).forEach(function (data) {
+          const stationItem = document.createElement("div");
+          stationItem.classList.add("col-lg-3", "col-md-5", "col-12");
+          stationItem.innerHTML = `
+            <div class="wheretorecycle-thumb">
+              <div class="wheretorecycle-info">
+                <small class="wheretorecycle-tag">${data.location}</small>
+                <h4 class="wheretorecycle-title">${data.details}</h4>
+              </div>
+
+              <div class="popup-image">
+                <img class="wheretorecycle-image img-fluid mx-auto" style="width: 250px;" alt="">
+              </div>
+            </div>
+          `;
+
+          // Create an image element and set its src attribute using the base64 string
+          const image = new Image();
+          image.src = `data:image/jpeg;base64, ${data.image}`;
+          image.classList.add("wheretorecycle-image");
+          stationItem.querySelector(".popup-image").appendChild(image);
+
+          recyclestations.appendChild(stationItem);
+        });
+
+  
+      // Add click event listener to show the modal with details
+      const modalButton = document.querySelectorAll('[data-toggle="modal"]');
+      modalButton.forEach(function (button) {
+        button.addEventListener("click", function () {
+          const details = this.getAttribute("data-details");
+          document.getElementById("modalDetails").textContent = details;
+        });
+      });
+
+      const spinner = document.getElementById("waitingrecyclestations");
+      if (spinner) {
+        spinner.remove();
+      }
+    } catch (error) {
+      console.log('Error getting recycle stations:', error);
+    }
+  }
+  
+
+  getRecycleStations();
   
   getMeals();
