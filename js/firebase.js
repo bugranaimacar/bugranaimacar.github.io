9a0d7b25869dc1d452117a7e3a184309395c8170
Buğra Naim Acar <47912949+bugranaimacar@users.noreply.github.com>
2023-05-31 00:24:54 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -36,49 +36,52 @@ async function getMeals() {
   }
 
   async function getRecycleStations() {
-    const url = "https://climate-change-in-tedu-campus-default-rtdb.europe-west1.firebasedatabase.app/recyclinglocations.json";
+    const url = "https://climate-change-in-tedu-campus-default-rtdb.europe-west1.firebasedatabase.app/recyclinglocations.json?orderBy=%22$key%22";
   
     try {
       const response = await fetch(url);
       const data = await response.json();
   
-      // Loop through the data and create HTML elements for each recycle station
-      // Loop through the data and create HTML elements for each recycle station
-        Object.values(data).forEach(function (data) {
-          const stationItem = document.createElement("div");
-          stationItem.classList.add("col-lg-3", "col-md-5", "col-12");
-          stationItem.innerHTML = `
-            <div class="wheretorecycle-thumb">
-              <div class="wheretorecycle-info">
-                <small class="wheretorecycle-tag">${data.location}</small>
-                <h4 class="wheretorecycle-title">${data.details}</h4>
-              </div>
-
-              <div class="popup-image">
-                <img class="wheretorecycle-image img-fluid mx-auto" style="width: 250px;" alt="">
-              </div>
-            </div>
-          `;
-
-          // Create an image element and set its src attribute using the base64 string
-          const image = new Image();
-          image.src = `data:image/jpeg;base64, ${data.image}`;
-          image.classList.add("wheretorecycle-image");
-          stationItem.querySelector(".popup-image").appendChild(image);
-
-          recyclestations.appendChild(stationItem);
-        });
-
+      const keyNames = Object.keys(data);
   
-      // Add click event listener to show the modal with details
-      const modalButton = document.querySelectorAll('[data-toggle="modal"]');
-      modalButton.forEach(function (button) {
-        button.addEventListener("click", function () {
-          const details = this.getAttribute("data-details");
-          document.getElementById("modalDetails").textContent = details;
-        });
-      });
+      keyNames.forEach(function (key) {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-3", "col-12", "col-md-5");
+  
+        const cardContent = document.createElement("div");
+        cardContent.classList.add("wheretorecycle-thumb");
+        cardContent.style.height = "500px";
+  
+        const titleContainer = document.createElement("div");
+        titleContainer.classList.add("d-flex", "align-items-center", "border-bottom", "mb-4", "pb-3");
+  
+        const title = document.createElement("h3");
+        title.classList.add("mb-0");
+        title.style.fontSize = "40px";
+        title.textContent = key;
+  
+        const description = document.createElement("p");
+        description.textContent = null;
+  
+        const button = document.createElement("a");
 
+        button.classList.add("custom-btn", "custom-border-btn", "btn", "mt-3", "mb-3");
+        button.textContent = "Show Stations";
+  
+        button.addEventListener("click", function () {
+          const details = data[key];
+          createRecycleCards(details); // Call a function to create the cards for the selected item
+        });
+  
+        description.appendChild(button);
+  
+        titleContainer.appendChild(title);
+        cardContent.appendChild(titleContainer);
+        cardContent.appendChild(description);
+        card.appendChild(cardContent);
+        recyclestations.appendChild(card);
+      });
+  
       const spinner = document.getElementById("waitingrecyclestations");
       if (spinner) {
         spinner.remove();
@@ -87,6 +90,59 @@ async function getMeals() {
       console.log('Error getting recycle stations:', error);
     }
   }
+  
+
+  
+  function createRecycleCards(details) {
+    const modalBody = document.getElementById("modalBody");
+    modalBody.innerHTML = ""; // Clear the modal body content
+  
+    details.forEach(function (item) {
+      const stationItem = document.createElement("div");
+      stationItem.classList.add("wheretorecycle-thumb");
+  
+      const stationInfo = document.createElement("div");
+      stationInfo.classList.add("wheretorecycle-info");
+  
+      const location = document.createElement("small");
+      location.classList.add("wheretorecycle-tag");
+      location.textContent = item.location;
+  
+      const title = document.createElement("h4");
+      title.classList.add("wheretorecycle-title");
+      title.textContent = item.details;
+  
+      stationInfo.appendChild(location);
+      stationInfo.appendChild(title);
+  
+      const popupImage = document.createElement("div");
+      popupImage.classList.add("popup-image");
+  
+      const image = new Image();
+      image.src = `data:image/jpeg;base64, ${item.image}`;
+      image.classList.add("wheretorecycle-image");
+      image.style.width = "250px";
+
+      popupImage.style.display = "flex";
+      popupImage.style.justifyContent = "center";
+      popupImage.style.alignItems = "center";
+
+  
+      popupImage.appendChild(image);
+
+      
+  
+      stationItem.appendChild(stationInfo);
+      stationItem.appendChild(popupImage);
+  
+      modalBody.appendChild(stationItem);
+    });
+  
+    // Open the modal
+    const modal = new bootstrap.Modal(document.getElementById("recycleStationsModal"));
+    modal.show();
+  }
+  
   
 
   getRecycleStations();
